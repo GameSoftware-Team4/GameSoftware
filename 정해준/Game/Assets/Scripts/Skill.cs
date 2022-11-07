b1a90892f6615d61ecee4baa115bf88dea9af148
jun-23 <jujng9699@gmail.com>
2022-11-07 17:14:34 +0900

    firstcommit

---
+++ 정해준/Game/Assets/Scripts/Skill.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour
+{
+    SpawningPool s;
+    MonsterAI m;
+    // Start is called before the first frame update
+    void Start()
+    {
+        s = GameObject.Find("SpawnManager").GetComponent<SpawningPool>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("Ãæµ¹");
+        if (s != null)
+        {
+            m = other.gameObject.GetComponent<MonsterAI>();
+            if(m != null && (Define.WorldObject.Monster == m.returnType()))
+            {
+
+                    Debug.Log("Monster");
+                    s.DestroyMonster();
+                    Destroy(other.gameObject);
+                
+            }
+
+        }
+    }
+
+}
