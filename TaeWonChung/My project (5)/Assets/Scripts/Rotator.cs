04528abd48a635a9aa64eadeed66c5aafd7ffc18
wellbeing98 <54866346+wellbeing98@users.noreply.github.com>
2022-11-09 15:55:29 +0900

    first

---
+++ TaeWonChung/My project (5)/Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+    public float rotationSpeed = 30f;
+    public bool flag= true;
+    void Update() {
+        if (flag)
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+    }
+}
