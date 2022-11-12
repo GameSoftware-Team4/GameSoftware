40cba08979f409a59101e5a0909a2f6d5a26559c
jun-23 <jujng9699@gmail.com>
2022-11-12 21:58:13 +0900

    1트

---
+++ 정해준/Game_ver2/Assets/Scripts/LivingEntity_jhj.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class LivingEntity_jhj : MonoBehaviour
+{
+    //생명체로 동작할 게임 오브젝트의 뼈대
+    //체력, 피해받음, 사망 기능
+
+    public float starting_hp_jhj = 100f; //시작 체력
+    public float hp_jhj { get; protected set; } //현재 체력
+    public bool dead_jhj { get; protected set; } //사망 상태
+    SpawningPool_jhj s_jhj;
+
+    public event Action onDeath;//사망시 발동할 이벤트
+
+    protected virtual void OnEnable() //생명체가 활성화 될 때 초기로 리셋
+    {
+        dead_jhj = false;
+
+        hp_jhj = starting_hp_jhj;
+
+    }
+
+    public virtual void OnDamage(float damage)
+    {
+        hp_jhj -= damage;
+        Debug.Log(hp_jhj);
+        if(hp_jhj <= 0 && !dead_jhj)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        if(onDeath != null)
+        {
+            onDeath();
+        }
+
+        dead_jhj = true;
+        Debug.Log("사망!");
+        s_jhj = GameObject.Find("SpawnManager").GetComponent<SpawningPool_jhj>();
+        if (s_jhj != null) { s_jhj.DestroyMonster(); }
+        Destroy(this.gameObject);
+    }
+}
---
+++ 정해준/Game_ver2/Assets/Scripts/Player_jhj.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_jhj : MonoBehaviour
+{
+    public GameObject rangeSkill_jhj; // 범위 스킬용 오브젝트 생성
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject skill = Instantiate(rangeSkill_jhj); //범위 스킬 오브젝트를 생성하고 일정시간후 프리펩을 제거
+            Destroy(skill,0.5f);
+        }
+        
+    }
+
+}
